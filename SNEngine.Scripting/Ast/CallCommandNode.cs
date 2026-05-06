a06e9459bd2c7a6d00b274a942f017f1c11a1883
Maxim <36982547+Siphoin@users.noreply.github.com>
2026-05-06 13:53:35 +0500

    add call function

---
+++ SNEngine.Scripting/Ast/CallCommandNode.cs
@@ -0,0 +1,31 @@
+﻿using Pidgin;
+using SNEngine.Scripting.Ast;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace SNEngine.Scripting.Ast;
+
+/// <summary>
+/// call hello()
+/// call calculateDamage()
+/// </summary>
+[SnCommand("call")]
+public sealed class CallCommandNode : CommandNode, IParsableCommand
+{
+    public string FunctionName { get; }
+
+    public CallCommandNode(string functionName)
+    {
+        FunctionName = functionName;
+    }
+
+    public static Parser<char, CommandNode> Parser { get; } =
+        String("call")
+            .Before(SkipWhitespaces)
+            .Then(CommonParsers.Identifier)
+            .Before(SkipWhitespaces)
+            .Before(Char('('))
+            .Before(SkipWhitespaces)
+            .Before(Char(')'))
+            .Select(name => (CommandNode)new CallCommandNode(name));
+}
---
+++ SNEngine.Scripting/CodeGen/CallCodeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SNEngine.Scripting.Ast;
+
+namespace SNEngine.Scripting.CodeGen;
+
+/// <summary>
+/// Generates: hello();
+/// </summary>
+[SnCodeGenerator(typeof(CallCommandNode))]
+public sealed class CallCodeGenerator : ICommandCodeGenerator
+{
+    public StatementSyntax Generate(CommandNode node)
+    {
+        if (node is not CallCommandNode call)
+            return SyntaxFactory.ParseStatement("// Invalid CallCommandNode");
+
+        return SyntaxFactory.ParseStatement($"{call.FunctionName}();");
+    }
+}

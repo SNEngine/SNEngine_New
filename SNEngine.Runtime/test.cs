b87c8ba7e149531df1095141bdd78a14da139341
Maxim <36982547+Siphoin@users.noreply.github.com>
2026-05-06 13:51:08 +0500

    Update test.cs

--- SNEngine.Runtime/test.cs
+++ SNEngine.Runtime/test.cs
@@ -11,7 +11,28 @@ public class testScene : SNScript
     {
         SNEngine.API.SNEngine.LoadEmptyScene();
         SetVar("myVar2", 15);
-        SetVar("MyVar25", GetVar("3.68"));
+        SetVar("MyVar25", 3.68);
+        SetVar("myvar80", "Hello World");
+        SetVar("playerHealth", 100);
+        SetVar("enemyLevel", 25);
+        SetVar("playerName", "Nagatoro");
+        SetVar("isAlive", true);
         SetVar("myvar80", GetVar("myVar25"));
+        SetVar("playerHealth", GetVar("enemyLevel"));
+        BackgroundAPI.Show("classroom");
+        CharacterAPI.Show("Nagatoro", "angry");
+    }
+
+    private void calculateDamage()
+    {
+        SetVar("tempDamage", 25);
+        SetVar("finalDamage", GetVar("tempDamage"));
+    }
+
+    private void levelUp()
+    {
+        SetVar("playerLevel", 10);
+        SetVar("playerHealth", GetVar("playerHealth") + 50);
+        SetVar("myvar80", "Level Up!");
     }
 }

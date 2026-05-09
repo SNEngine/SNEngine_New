8bb66c4a898654a8fdb6c143f07d12844ee0499c
Maxim <36982547+Siphoin@users.noreply.github.com>
2026-05-09 17:54:26 +0500

    select Vue for Studio

--- SNEngine.Studio/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  runScript: (code) => ipcRenderer.invoke('run-script', code),
-  saveFile: (path, content) => ipcRenderer.invoke('save-file', path, content)
-})
-
-console.log('✅ Preload script loaded')
